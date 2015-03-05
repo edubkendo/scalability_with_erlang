e97dfa28c5c719ffda43a41f03a1955aa318ee14
Eric West <esw9999@gmail.com>
2015-03-05 16:31:52 -0600

    Add Ping timeouts exercise.

--- three/ping/lib/ping.ex
+++ three/ping/lib/ping.ex
@@ -1,2 +1,20 @@
 defmodule Ping do
+  use GenServer
+
+  @timeout 5000
+
+  def init(_args), do: {:ok, :null, @timeout}
+
+  def handle_call(:start, _from, loop_data) do
+    {:reply, :started, loop_data, @timeout}
+  end
+  def handle_call(:pause, _from, loop_data) do
+    {:reply, :paused, loop_data}
+  end
+
+  def handle_info(:timeout, loop_data) do
+    {_hour, _min, sec} = :erlang.time
+    IO.puts "#{sec}"
+    {:noreply, loop_data, @timeout}
+  end
 end
